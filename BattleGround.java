026eaabc0634ed0b6cb9d532a399932a01245207
bienfromage <100codemaster@gmail.com>
2016-12-29 21:01:46 -0500

    Update BattleGround.java

--- BattleGround.java
+++ BattleGround.java
@@ -9,7 +9,7 @@ import javax.imageio.*;
 import java.util.Random;
 import java.util.ArrayList;
 
-public class BattleGround extends JPanel implements ActionListener, KeyListener{
+public class BattleGround extends JPanel implements ActionListener{
     Timer tm = new Timer(5, this);
     int humanX = 100, velHumanX = 0, humanY = 100, velHumanY = 0, humanHeight = 0, humanWidth = 0, humanDeathSequence = 0;
     BufferedImage humanImage;
@@ -30,10 +30,6 @@ public class BattleGround extends JPanel implements ActionListener, KeyListener{
     boolean bulletsActive = false, humanDied = false, alienDied = false;
     int asteroids = 0;
     public BattleGround(){
-        addKeyListener(this);
-        setFocusable(true);
-        setFocusTraversalKeysEnabled(false);
-        requestFocusInWindow();
 
         URL resource = getClass().getResource("HumanShip.png");//get human fighter
         try{
@@ -53,7 +49,7 @@ public class BattleGround extends JPanel implements ActionListener, KeyListener{
         humanWidth = humanImage.getWidth();
         alienHeight = alienImage.getHeight();
         alienWidth = alienImage.getWidth();
-        
+
         Random cow = new Random();
         humanX = cow.nextInt(width-humanWidth);
         humanY = cow.nextInt(height-humanHeight);
@@ -61,6 +57,64 @@ public class BattleGround extends JPanel implements ActionListener, KeyListener{
             alienX = cow.nextInt(width-alienWidth);
             alienY = cow.nextInt(height-alienHeight);
         }while((alienX+alienWidth >= humanX && alienX <= humanX+humanWidth && alienY+alienHeight >= humanY && alienY <= humanY+humanHeight));
+
+        //Human Input
+        getInputMap().put(KeyStroke.getKeyStroke("released ENTER"), "enter without modifiers");
+        getActionMap().put("enter without modifiers", new AbstractAction(){public void actionPerformed(ActionEvent a){fire(1);}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("pressed UP"), "up arrow");
+        getActionMap().put("up arrow", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velHumanY = -4;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("released UP"), "up arrow released");
+        getActionMap().put("up arrow released", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velHumanY = 0;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("pressed LEFT"), "left arrow");
+        getActionMap().put("left arrow", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velHumanX = -4;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("released LEFT"), "left arrow released");
+        getActionMap().put("left arrow released", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velHumanX = 0;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("pressed DOWN"), "down arrow");
+        getActionMap().put("down arrow", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velHumanY = 4;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("released DOWN"), "down arrow released");
+        getActionMap().put("down arrow released", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velHumanY = 0;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("pressed RIGHT"), "right arrow");
+        getActionMap().put("right arrow", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velHumanX = 4;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("released RIGHT"), "right arrow released");
+        getActionMap().put("right arrow released", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velHumanX = 0;}}});
+
+        //Alien Input
+        getInputMap().put(KeyStroke.getKeyStroke("released SPACE"), "space without modifiers");
+        getActionMap().put("space without modifiers", new AbstractAction(){public void actionPerformed(ActionEvent a){fire(2);}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("pressed W"), "up");
+        getActionMap().put("up", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velAlienY = -4;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("released W"), "up released");
+        getActionMap().put("up released", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velAlienY = 0;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("pressed A"), "left");
+        getActionMap().put("left", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velAlienX = -4;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("released A"), "left released");
+        getActionMap().put("left released", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velAlienX = 0;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("pressed S"), "down");
+        getActionMap().put("down", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velAlienY = 4;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("released S"), "down released");
+        getActionMap().put("down released", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velAlienY = 0;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("pressed D"), "right");
+        getActionMap().put("right", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velAlienX = 4;}}});
+
+        getInputMap().put(KeyStroke.getKeyStroke("released D"), "right released");
+        getActionMap().put("right released", new AbstractAction(){public void actionPerformed(ActionEvent a){if(!humanDied){velAlienX = 0;}}});
+
+
         tm.start();
     }
 
@@ -91,88 +145,6 @@ public class BattleGround extends JPanel implements ActionListener, KeyListener{
         repaint();
     }
 
-    public void keyPressed(KeyEvent e){
-        int c = e.getKeyCode();
-        if(!humanDied) {
-            if (c == KeyEvent.VK_LEFT) {//if left arrow key pressed
-                velHumanX = -4;
-
-            }
-            if (c == KeyEvent.VK_UP) {
-                velHumanY = -4;
-            }
-            if (c == KeyEvent.VK_RIGHT) {
-                velHumanX = 4;
-            }
-            if (c == KeyEvent.VK_DOWN) {
-                velHumanY = 4;
-            }
-            if (c == 10) {//"enter" to fire
-                fire(1);
-            }
-        }
-        //alien controls
-        if(!alienDied) {
-            if (c == 65) {//a
-                velAlienX = -4;
-            }
-            if (c == 87) {//w
-                velAlienY = -4;
-            }
-            if (c == 68) {//d
-                velAlienX = 4;
-            }
-            if (c == 83) {//s
-                velAlienY = 4;
-            }
-            if (c == 32) {//"space" to fire
-                fire(2);
-            }
-        }
-    }
-
-    public void keyReleased(KeyEvent e){
-        int y= e.getKeyCode();
-        if(!humanDied) {
-            if (y== KeyEvent.VK_LEFT) {//if left arrow key pressed
-                if(!(velHumanX == 4))
-                    velHumanX = 0;
-            }
-            if (y== KeyEvent.VK_UP) {
-                if(!(velHumanY == 4))
-                    velHumanY = 0;
-            }
-            if (y== KeyEvent.VK_RIGHT) {
-                if(!(velHumanX == -4))
-                    velHumanX =0;
-            }
-            if (y== KeyEvent.VK_DOWN) {
-                if(!(velHumanY == -4))
-                    velHumanY = 0;
-            }
-        }
-        if(!alienDied) {
-            if (y== 65) {//a
-                if(!(velAlienX == 4))
-                    velAlienX = 0;
-            }
-            if (y== 87) {//w
-                if(!(velAlienX == 4))
-                    velAlienY = 0;
-            }
-            if (y== 68) {//d
-                if(!(velAlienX == -4))
-                    velAlienX =0;
-            }
-            if (y== 83) {//s
-                if(!(velAlienX == -4))
-                    velAlienY = 0;
-            }
-        }
-    }
-
-    public void keyTyped(KeyEvent e){}
-
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         g.setColor(Color.BLACK);
@@ -187,12 +159,12 @@ public class BattleGround extends JPanel implements ActionListener, KeyListener{
             }
             asteroids = fromage.nextInt(5) + 2;//randomly generate asteroids
 
-           int rockX, rockY, rockA, rockB;
+            int rockX, rockY, rockA, rockB;
 
-           do{
+            do{
                 rockX = fromage.nextInt(width)- 180;
                 rockY = fromage.nextInt(height) - 180;
-           } while((rockX > humanX - 181 && rockX < humanX + humanWidth && rockY < humanY + humanHeight && rockY > humanY - 181) || (rockX > alienX - 181 && rockX < alienX + alienWidth && rockY < alienY + alienHeight && rockY > alienY - 181) || (rockX < 0 || rockY <0));
+            } while((rockX > humanX - 181 && rockX < humanX + humanWidth && rockY < humanY + humanHeight && rockY > humanY - 181) || (rockX > alienX - 181 && rockX < alienX + alienWidth && rockY < alienY + alienHeight && rockY > alienY - 181) || (rockX < 0 || rockY <0));
 
             asteroidX.add(rockX);//pt 0
             asteroidX.add(rockX - fromage.nextInt(41)+20);//1
@@ -214,12 +186,12 @@ public class BattleGround extends JPanel implements ActionListener, KeyListener{
             asteroidY.add(rockY + fromage.nextInt(31)+60);//8
             asteroidY.add(rockY + fromage.nextInt(31)+40);//9
             asteroidY.add(rockY);//10
-            
+
             do{
                 rockA = fromage.nextInt(width) - 180;
                 rockB = fromage.nextInt(height) - 180;
             }while((rockA > humanX - 181 && rockA < humanX + humanWidth && rockB < humanY + humanHeight && rockB > humanY - 181) || (rockA > alienX - 181 && rockA < alienX + alienWidth && rockB < alienY + alienHeight && rockB > alienY - 181) || (rockA < 0 || rockB <0));
-            
+
             asteroidX.add(rockA);//10
             asteroidX.add(rockA-(fromage.nextInt(21) + 20));//11
             asteroidX.add(rockA-(fromage.nextInt(31) + 20));//12
@@ -240,19 +212,19 @@ public class BattleGround extends JPanel implements ActionListener, KeyListener{
             asteroidY.add(rockB+ fromage.nextInt(31) + 5);
             asteroidY.add(rockB+ fromage.nextInt(31));
             asteroidY.add(rockB);
-            
+
             Polygon asteroid = new Polygon();
             for(int i = 0; i < 10; i++) {
                 asteroid.addPoint(asteroidX.get(i), asteroidY.get(i));
             }
             g.setColor(Color.GRAY);
             g.fillPolygon(asteroid);
-            
+
             Polygon asteroid1 = new Polygon();
             for(int i=10;i<20;i++)
                 asteroid1.addPoint(asteroidX.get(i), asteroidY.get(i));
             g.fillPolygon(asteroid1);
-            
+
             fieldReady = true;
         }else{
             g.setColor(Color.WHITE);
