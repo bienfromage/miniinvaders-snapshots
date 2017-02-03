e77269d3deefb74e937ae13259e58bfec6a657cd
bienfromage <100codemaster@gmail.com>
2017-02-02 20:30:02 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -47,6 +47,15 @@ public class Main extends JPanel implements MouseListener{
         frame.add(background);
         frame.setVisible(true);
         frame.setResizable(false);
+        try{
+            File file = new File("score.txt");
+            FileWriter fw = new FileWriter(file,false);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write("");
+            bw.close();
+        }catch(IOException e){
+            System.out.println(e);
+        }
     }
 
     public void paintComponent(Graphics g){
@@ -63,7 +72,7 @@ public class Main extends JPanel implements MouseListener{
         g.fillRect(width/2-120,height/2+60,90,30);//tutorial button
         g.fillRect(width/2+24,height/2+60,105,30);//scores button
         g.fillRect(width/2-100,height/2-40,200,80);//play button
-        
+
         g.setColor(Color.BLACK);
         g.drawString("CONTROLS", width/2-110, height/2+80);//tutorial
         g.drawString("LOCAL SCORES", width/2+30, height/2+80);//scores
